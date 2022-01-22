5fde3afeac85c7cb0fcec639a39477ac3c0ba143
Vitaly Borodulin <rainbowf@mail.ru>
2022-01-22 17:01:17 +0500

    Fix form_teams function

--- project_automatization_bot/management/commands/form_teams.py
+++ project_automatization_bot/management/commands/form_teams.py
@@ -57,23 +57,71 @@ class Command(BaseCommand):
         
         sorted_students = sort_students_by_available_time(time_windows)
         # pprint(sorted_students)
-        
-        def add_student_to_temp_team(
-                teams,
-                student,
-                formed_teams,
-                time,
-                time_windows,
-                # first_level_priority,
-                # second_level_priority
-            ):
+
+        def handle_distribution(
+            teams,
+            student,
+            formed_teams,
+            time,
+            time_windows,
+            first_level_priority,
+            second_level_priority,
+            is_added):
+            for team_id, time_window in first_level_priority.items():
+                if (student.time_call.get(time_window) is not None
+                        and student.time_call.get(time_window)
+                        and teams[team_id]['level'] != student.status
+                        and not (teams[team_id]['level'] != student.status
+                                 and (teams[team_id]['level'] == 'junior'
+                                 or student.status == 'junior'))):
+                    teams[team_id]['third'] = student.tg_chat_id
+                    formed_teams[team_id] = teams.pop(team_id)
+                    time_windows[time] -= 1
+                    is_added = 1
+                    print('add third')
+                    print(f'Оставшиеся окна: {time_windows}')
+                    del first_level_priority[team_id]
+                    print('first_priority')
+                    pprint(first_level_priority)
+                    return is_added
+            
+            for team_id, time_window in second_level_priority.items():
+                if (student.time_call.get(time_window) is not None
+                        and student.time_call.get(time_window)
+                        and teams[team_id]['level'] != student.status
+                        and not (teams[team_id]['level'] != student.status
+                                 and (teams[team_id]['level'] == 'junior'
+                                 or student.status == 'junior'))):
+                    teams[team_id]['second'] = student.tg_chat_id
+                    is_added = 1
+                    print('add second')
+                    del second_level_priority[team_id]
+                    first_level_priority[team_id] = time
+                    first_level_priority = sorted(
+                        first_level_priority.items(),
+                        key=lambda x: x[1]
+                    )
+                    print('first_priority')
+                    pprint(first_level_priority)
+                    print('second_priority')
+                    pprint(second_level_priority)
+                    return is_added
+
             for team_id in teams:
                 if teams[team_id]['start_time'] == time:
                     if teams[team_id].get('level') is None:
                         teams[team_id]['first'] = student.tg_chat_id
                         teams[team_id]['level'] = student.status
+                        is_added = 1
                         print('add first')
-                        break
+                        second_level_priority[team_id] = time
+                        second_level_priority = sorted(
+                            second_level_priority.items(),
+                            key=lambda x: x[1]
+                        )
+                        print('second_priority')
+                        pprint(second_level_priority)
+                        return is_added
                     else:
                         if (teams[team_id]['level'] != student.status
                                 and (teams[team_id]['level'] == 'junior'
@@ -82,15 +130,99 @@ class Command(BaseCommand):
                         else:
                             if teams[team_id].get('second') is None:
                                 teams[team_id]['second'] = student.tg_chat_id
+                                is_added = 1
                                 print('add second')
-                                break
+                                del second_level_priority[team_id]
+                                first_level_priority[team_id] = time
+                                first_level_priority = sorted(
+                                    first_level_priority.items(),
+                                    key=lambda x: x[1]
+                                )
+                                print('first_priority')
+                                pprint(first_level_priority)
+                                print('second_priority')
+                                pprint(second_level_priority)
+                                return is_added
                             else:
                                 teams[team_id]['third'] = student.tg_chat_id
                                 formed_teams[team_id] = teams.pop(team_id)
                                 time_windows[time] -= 1
+                                is_added = 1
                                 print('add third')
                                 print(f'Оставшиеся окна: {time_windows}')
-                                break
+                                del first_level_priority[team_id]
+                                print('first_priority')
+                                pprint(first_level_priority)
+                                return is_added
+
+        def add_student_to_temp_team(
+            teams,
+            student,
+            formed_teams,
+            time,
+            time_windows,
+            first_level_priority,
+            second_level_priority
+        ):
+            is_added = 0
+            pprint(first_level_priority)
+            pprint(second_level_priority)
+            print(time)
+            if (first_level_priority 
+                    and (time in first_level_priority.values())):
+                for team_id in first_level_priority:
+                    if (teams[team_id]['level'] != student.status
+                            and (teams[team_id]['level'] == 'junior'
+                            or student.status == 'junior')):
+                        continue
+                    else:
+                        teams[team_id]['third'] = student.tg_chat_id
+                        formed_teams[team_id] = teams.pop(team_id)
+                        time_windows[time] -= 1
+                        is_added = 1
+                        print('add third')
+                        print(f'Оставшиеся окна: {time_windows}')
+                        del first_level_priority[team_id]
+                        print('first_priority')
+                        pprint(first_level_priority)
+                        break
+            if (second_level_priority
+                    and (time in second_level_priority.values())
+                    and not is_added):
+                for team_id in second_level_priority:
+                    if time in second_level_priority.values():
+                        if (teams[team_id]['level'] != student.status
+                                and (teams[team_id]['level'] == 'junior'
+                                or student.status == 'junior')):
+                            continue
+                        else:
+                            teams[team_id]['second'] = student.tg_chat_id
+                            is_added = 1
+                            print('add second')
+                            del second_level_priority[team_id]
+                            first_level_priority[team_id] = time
+                            first_level_priority = sorted(
+                                first_level_priority.items(),
+                                key=lambda x: x[1]
+                            )
+                            print('first_priority')
+                            pprint(first_level_priority)
+                            print('second_priority')
+                            pprint(second_level_priority)
+                            break
+            if not is_added:
+                print('HERE')
+                is_added = handle_distribution(
+                    teams=teams,
+                    student=student,
+                    formed_teams=formed_teams,
+                    time=time,
+                    time_windows=time_windows,
+                    first_level_priority=first_level_priority,
+                    second_level_priority=second_level_priority,
+                    is_added=is_added
+                )
+            return is_added
 
         def form_teams(sorted_students, time_windows):
             out_of_project = list()
@@ -116,24 +248,25 @@ class Command(BaseCommand):
                                 formed_teams=formed_teams,
                                 time=time,
                                 time_windows=time_windows,
+                                first_level_priority=first_level_priority_teams,
+                                second_level_priority=second_level_priority_teams
                             )
-                            print(f'student {selected_student.tg_chat_id} was added to new team')
                 else:
                     for time in selected_student.time_call:
                         if (time_windows.get(time) is not None
                                 and time_windows.get(time)
                                 and selected_student.time_call[time]):
-                            add_student_to_temp_team(
+                            is_added = add_student_to_temp_team(
                                 teams=teams,
                                 student=selected_student,
                                 formed_teams=formed_teams,
                                 time=time,
                                 time_windows=time_windows,
-                                # first_level_priority=first_level_priority_teams,
-                                # second_level_priority=second_level_priority_teams
+                                first_level_priority=first_level_priority_teams,
+                                second_level_priority=second_level_priority_teams
                             )
-                            print(f'student {selected_student.tg_chat_id} was added to team')
-                            break
+                            if is_added:
+                                break
 
             return out_of_project, teams, formed_teams
         
