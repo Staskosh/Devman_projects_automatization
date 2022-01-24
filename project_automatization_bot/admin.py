de6fd65499e1ff8ac2d333ed197b09c14177a35e
turim0 <ivan_medvedev_1996@mail.ru>
2022-01-24 19:43:46 +0300

    add script buttons, delete student_distribution model

--- project_automatization_bot/admin.py
+++ project_automatization_bot/admin.py
@@ -7,14 +7,15 @@ from .models import (
     Project_manager,
     Team,
     Project,
-    Student_distribution,
     IncompleteTeam
 )
 from distribution import main
+from project_automatization_bot.management.commands import notify
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
+    change_list_template = 'students_change_list.html'
     list_display = (
         'id',
         'name',
@@ -24,6 +25,25 @@ class StudentAdmin(admin.ModelAdmin):
         'is_far_east',
         'is_out_of_project'
     )
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('notify/', self.send_notifications),
+            path('distribute/', self.make_distribution)
+        ]
+        return my_urls+urls
+
+
+    def send_notifications(self, request):
+        notify.main()
+        self.message_user(request, 'Уведомления разосланы')
+        return redirect('../')
+
+    def make_distribution(self, request):
+        main()
+        self.message_user(request, 'Распределение произведено')
+        return redirect('../')
+
 
 
 @admin.register(Project_manager)
@@ -51,23 +71,3 @@ class TeamAdmin(admin.ModelAdmin):
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'project_manager', 'students')
 
-
-@admin.register(Student_distribution)
-class Student_distributionAdmin(admin.ModelAdmin):
-    list_display = ('activate_button',)
-
-    def get_urls(self):
-        urls = super().get_urls()
-        my_urls = [
-            path(
-                '<int:pk>/',
-                self.make_distribution,
-                name='admin_make_distribution'
-            )
-        ]
-        return my_urls + urls
-
-    def make_distribution(self, request, pk):
-        main()
-        self.message_user(request, "Распределение произведено")
-        return redirect("../../team/")
